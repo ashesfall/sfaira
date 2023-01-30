19384ebac04f5145376606f617dfd617b1730f04
Michael Murray <ashesfall@almostrealism.com>
2023-01-30 11:47:27 -0800

    fix: image read test.

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from PIL import Image
 from skimage import io
 
-print(io.imread("Landsat_044033_20180719.tif"))
+labelData = io.imread("Landsat_044033_20210711.tif")
+print(labelData.shape)
 
-# labelData = Image.open("Landsat_044033_20180719.tif").convert("RGB")
-# image = Image.open("LC08_L1TP_044033_20180719_20200831_02_T1_B6.TIF").convert("RGB")
-
-# print(test)
+imageData = io.imread("LC08_L1TP_044033_20210711_20210720_02_T1_B6.TIF")
+print(imageData.shape)
